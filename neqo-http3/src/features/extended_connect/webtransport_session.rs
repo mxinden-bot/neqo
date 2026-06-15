01814eec75e30a3fdb6d41efe8cfab4ccd74ff70
Randell Jesup <rjesup@mozilla.com>
2026-06-15 15:20:09 +0000

    Use FxHashSet for WebTransport session stream and group sets

--- neqo-http3/src/features/extended_connect/webtransport_session.rs
+++ neqo-http3/src/features/extended_connect/webtransport_session.rs
@@ -5,7 +5,6 @@
 // except according to those terms.
 
 use std::{
-    collections::HashSet,
     fmt::{self, Display, Formatter},
     mem,
     time::Instant,
@@ -13,6 +12,7 @@ use std::{
 
 use neqo_common::{Bytes, Encoder, Header, Role, qtrace};
 use neqo_transport::{Connection, Error as TransportError, StreamId};
+use rustc_hash::FxHashSet as HashSet;
 use sfv::{BareItem, Item, Parser};
 
 use crate::{
@@ -66,10 +66,10 @@ impl Session {
     ///
     /// Returns an error if the ID is already in use.
     pub(crate) fn register_send_group(&mut self, id: SendGroupId) -> Res<()> {
-        if !self.send_groups.insert(id) {
-            return Err(Error::InvalidState);
-        }
-        Ok(())
+        self.send_groups
+            .insert(id)
+            .then_some(())
+            .ok_or(Error::InvalidState)
     }
 
     /// Validate that a send group belongs to this session.
